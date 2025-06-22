0cbc896703a6f1fff026220383a217019709a59d
smdzhm <2741666788@qq.com>
2025-06-22 13:32:18 +0800

    first commit3

--- pages/pinglun/index.ts
+++
@@ -1,8 +0,0 @@
-import Twikoo from './components/Twikoo.vue';
-
-export default {
-  Layout: DefaultLayout,
-  enhanceApp({ app, router, siteData }) {
-    app.component("Twikoo", Twikoo);
-  },
-} satisfies Theme;
